8388e79c03a3ab249782b32510d6c1eaa8ef3826
JosseGonnza <accion.quevedo@hotmail.com>
2024-05-19 20:33:51 +0100

    Code smells Dead code

--- src/farm/tool.ts
+++ src/farm/tool.ts
@@ -9,31 +9,6 @@ export class Tool {
     this.improvement = improvement
   }
 
-  /*improveTool(toolMaterialType: string, bars: Material, woods: Material) {
-    if(toolMaterialType === 'cooper') {
-      if(this.material !== 'stone') {
-        throw new Error('Cannot improve tool to cooper')
-      }
-      if(bars.name != 'cooper bar') {
-        throw new Error('You need cooper bars to improve this tool to cooper')
-      }
-      if(bars.quantity < 5 || woods.quantity < 5) {
-        throw new Error('Not enough materials to improve to cooper')
-      }
-      this.improveToCooper(bars, woods)
-    } else if(toolMaterialType === 'steel') {
-      if(this.material !== 'cooper') {
-        throw new Error('Cannot improve tool to steel')
-      }
-      if(bars.name != 'iron bar') {
-        throw new Error('You need iron bars to improve this tool to steel')
-      }
-      if(bars.quantity < 5 || woods.quantity < 5) {
-        throw new Error('Not enough materials to improve to steel')
-      }
-      this.improveToSteel(bars, woods)
-    }
-  }*/
 
   improveToCooper(cooperBars: Material, woods: Material) {
     if(this.material !== 'stone') {
